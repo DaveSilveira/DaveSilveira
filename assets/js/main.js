d0a0d3702fb4cfc67922d8cc069db1dd519bef87
David Jordânio Silveira <119808305+DaveSilveira@users.noreply.github.com>
2025-12-13 07:17:27 -0300

    adjusting the project grid

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,22 @@
-const grid = document.querySelector('.work_grid')
-grid.innerHTML = '<p>teste</p>'
+const grid = document.querySelector('.work_grid');
 
 function criaDiv(){
     const criarDiv = document.createElement('div');
     return criarDiv;
 }
 
-const gridWorks = [
+const gridWorks = [ //here will add my projects 
 {name: 'hematologicCount', link: "#", backrgound_img: "#"},
 {name: 'passwordGenerator', link: "#", backrgound_img: "#"},
-]
+];
 
-grid.addEventListener('onload', function(){
-    grid.innerHTML = 'teste2'
+document.addEventListener('DOMContentLoaded', function(){
+    for(let i = 0; i < gridWorks.length; i++){
+        
+    let {name, link, backrgound_img} = gridWorks[i];
+    let workWindow = criaDiv();
+    grid.appendChild(workWindow);
+    workWindow.style.backgroundImage = backrgound_img;
+
+    }
 });
