fc16e60ae256514b52a7cc5cd9bb8becda9c296a
David Jordânio Silveira <119808305+DaveSilveira@users.noreply.github.com>
2025-12-13 07:03:36 -0300

    adjusting design site

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,16 @@
-const grid = document.querySelector('.gridTrabalhos')
-
-
+const grid = document.querySelector('.work_grid')
 grid.innerHTML = '<p>teste</p>'
+
+function criaDiv(){
+    const criarDiv = document.createElement('div');
+    return criarDiv;
+}
+
+const gridWorks = [
+{name: 'hematologicCount', link: "#", backrgound_img: "#"},
+{name: 'passwordGenerator', link: "#", backrgound_img: "#"},
+]
+
+grid.addEventListener('onload', function(){
+    grid.innerHTML = 'teste2'
+});
